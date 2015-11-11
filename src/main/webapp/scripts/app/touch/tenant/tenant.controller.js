cf47cd3f437f69a25122b83b8a9cc65460d6862c
Ming Lee <ming.lee@fis.com.hk>
2015-11-12 01:44:56 +0800

    removed the tenant.directive
    added category search function

--- src/main/webapp/scripts/app/touch/tenant/tenant.controller.js
+++ src/main/webapp/scripts/app/touch/tenant/tenant.controller.js
@@ -4,7 +4,7 @@ angular.module('wayfindingApp')
     .controller('TouchTenantController', function ($scope, $rootScope, TouchService, ParseLinks, $http) {
         $scope.tenants = [];
         $scope.page = 0;
-
+        $scope.tenantSelected = '';
         $scope.data = {
             selectedIndex: 0
         };
@@ -29,19 +29,38 @@ angular.module('wayfindingApp')
             $scope.searchByFloorKeyboardLayout = data;
         });
 
+        $scope.searchModel = {
+            name: '',
+            location : '',
+            category : '',
+        };
         $scope.textKeyPressed = function(value, action){
-            $scope.someInput = value;
-            $rootScope.$broadcast('textKeyPressed', $scope.someInput, action);
-        }
+            if(action ==='del'){
+                $scope.searchModel.name = $scope.searchModel.name.substr(0, $scope.searchModel.name.length-1)
+            }else{
+                $scope.searchModel.name = $scope.searchModel.name + value;
+            }
+            $scope.searchModel.category = '';
+            $scope.searchModel.location = '';
+        };
         $scope.categoryKeyPressed = function(value, action){
-            $scope.someInput = value;
-            $rootScope.$broadcast('categoryKeyPressed', $scope.someInput, action);
-        }
+            $scope.searchModel.name = '';
+            $scope.searchModel.category = value;
+            $scope.searchModel.location = '';
+        };
         $scope.floorKeyPressed = function(value, action){
-            $scope.someInput = value;
-            $rootScope.$broadcast('floorKeyPressed', $scope.someInput, action);
-        }
-        
+            $scope.searchModel.name = '';
+            $scope.searchModel.category = '';
+            if(action ==='del'){
+                $scope.searchModel.location = $scope.searchModel.location.substr(0, $scope.searchModel.location.length-1)
+            }else{
+                $scope.searchModel.location = $scope.searchModel.location + value;
+            }
+        };
+        $scope.selectTenant = function(tenant){
+            $scope.tenantSelected = tenant;
+        };
+
         var createFloor = function(n) {
             var floors = [];
             for (var i = 1; i <= n; i++) {
@@ -49,11 +68,11 @@ angular.module('wayfindingApp')
             }
             return floors;
         };
-        
+
         var init = function() {
             $scope.floors = createFloor(59);
         };
-        
+
         init();
     });
 
