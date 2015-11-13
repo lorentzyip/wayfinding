3051aecf0f46070f0d8616e25b11731cb912cae7
Lorentz Yip <lorentz.yip@gmail.com>
2015-11-13 22:49:33 +0800

    complete map; add pointer;

--- src/main/webapp/scripts/app/touch/tenant/tenant.controller.js
+++ src/main/webapp/scripts/app/touch/tenant/tenant.controller.js
@@ -3,8 +3,10 @@
 angular.module('wayfindingApp')
     .controller('TouchTenantController', function ($scope, $rootScope, TouchService, ParseLinks, $http) {
         $scope.tenants = [];
+        $scope.floorPlanNums = [];
         $scope.page = 0;
-        $scope.tenantSelected = '';
+        $scope.selectedFloor = -1;
+        $scope.tenantSelected = null;
         $scope.tabs = {
             selectedIndex: 0
         };
@@ -14,7 +16,53 @@ angular.module('wayfindingApp')
         $scope.previous = function() {
             $scope.data.selectedIndex = Math.max($scope.data.selectedIndex - 1, 0);
         };
-
+        
+        $scope.showFloorPlan = function(floorPlan) {
+            var result = false;
+            
+            switch (floorPlan) {
+                case 11:
+                    result = ($scope.selectedFloor > 0) && ($scope.selectedFloor <= 11);
+                    break;
+                case 12:
+                    result = $scope.selectedFloor == 12;
+                    break;
+                case 15:
+                    result = ($scope.selectedFloor > 12) && ($scope.selectedFloor <= 15);
+                    break;
+                case 16:
+                    result = $scope.selectedFloor == 16;
+                    break;
+                case 17:
+                    result = $scope.selectedFloor == 17;
+                    break;
+                case 19:
+                    result = ($scope.selectedFloor > 17) && ($scope.selectedFloor <= 19);
+                    break;
+                case 20:
+                    result = $scope.selectedFloor == 20;
+                    break;
+                case 21:
+                    result = $scope.selectedFloor == 21;
+                    break;
+                case 22:
+                    result = $scope.selectedFloor == 22;
+                    break;
+                case 23:
+                    result = $scope.selectedFloor == 23;
+                    break;
+                case 25:
+                    result = ($scope.selectedFloor > 23) && ($scope.selectedFloor <= 25);
+                    break;
+                case 26:
+                    result = $scope.selectedFloor == 26;
+                    break;
+                default:
+                    break;
+            }
+            
+            return result;
+        };
 
         $http.get('assets/jsons/tenant/Tenant_Infor.json').success(function(data) {
             $scope.tenants = data.Tenant_Infor;
@@ -27,6 +75,15 @@ angular.module('wayfindingApp')
         });
         $http.get('assets/jsons/tenant/searchByFloorKeyboardLayout.json').success(function(data) {
             $scope.searchByFloorKeyboardLayout = data;
+        });
+        $http.get('assets/jsons/tenant/floorPlans.json').success(function(data) {
+            $scope.floorPlans = data; 
+            for (var i = 0; i < $scope.floorPlans.length; i++) {
+                $scope.floorPlanNums.push($scope.floorPlans[i].floorNum);
+            }
+            $scope.floorPlanNums = $scope.floorPlanNums.sort(function (a, b) { 
+                return a - b;
+            });
         });
 
         $scope.searchModel = {
@@ -63,11 +120,52 @@ angular.module('wayfindingApp')
         };
         $scope.selectTenant = function(tenant){
             $scope.tenantSelected = tenant;
+            $scope.selectedFloor = Number(tenant.floor);
+            $scope.selectedRoom = tenant.RoomNO;
+            $scope.selectedFloorPlan = $scope.getFloorPlan($scope.selectedFloor);
+            $scope.selectedRoomCoord = $scope.getRoomCoord($scope.selectedFloorPlan, $scope.selectedRoom);
         };
         $scope.reset = function(){
             $scope.searchModel.Nameen_us = '';
             $scope.searchModel.RoomNO = '';
             $scope.searchModel.ShopCategory = '';
+        };
+        
+        $scope.getRoomCoord = function(floorPlan, roomNumStr /* string */) {
+            var coord = { display: 'none', top: '0', left: '0'};
+            
+            if (floorPlan == null || room == '') return coord;
+            
+            for (var i = 0; i < floorPlan.rooms.length; i++) {
+                var room /* number */ = floorPlan.rooms[i];
+                if (room.roomNum.toString() == roomNumStr) {
+                    coord = { display: 'block', top: room.coord.top, left: room.coord.left };
+                }
+            }
+            
+            return coord;
+        };
+        
+        $scope.getFloorPlan = function(floor) {
+            if ($scope.floorPlans == null || floor < 0) return null;
+            
+            /* assume floorPlanNums sorted asc */
+            var result = $scope.floorPlanNums[$scope.floorPlanNums.length - 1];
+            
+            for (var i = 0; i < $scope.floorPlanNums.length; i++) {
+                var floorPlanNum = $scope.floorPlanNums[i];
+                if (floor <= floorPlanNum) {
+                    result = floorPlanNum;
+                    break;
+                }
+            }
+            
+            for (var i = 0; i < $scope.floorPlans.length; i++) {
+                var floorPlan = $scope.floorPlans[i]
+                if (result == floorPlan.floorNum) return floorPlan
+            }
+            
+            return null;
         };
 
         $scope.$watch('tabs.selectedIndex', function(current, old) {
